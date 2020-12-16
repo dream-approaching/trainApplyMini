384cf411469e1eb69eb5771043e83d847dcc5be8
zhengjinshou <zhengjinshou@qtrade.com.cn>
2020-12-16 23:47:25 +0800

    .

--- client/src/pages/index/index.jsx
+++ client/src/pages/index/index.jsx
@@ -1,19 +1,56 @@
-import Taro, { useState } from "@tarojs/taro";
-import { AtTabBar } from "taro-ui";
+import Taro, {
+  useState,
+  useShareAppMessage,
+  useShareTimeline
+} from "@tarojs/taro";
+import { AtTabs, AtTabsPane, AtButton } from "taro-ui";
 import { View } from "@tarojs/components";
 import styles from "./index.module.less";
 
 export default function Index() {
   const [current, setCurrent] = useState(0);
 
-  console.log("%c zjs styles:", "color: #0e93e0;background: #aaefe5;", styles);
+  useShareAppMessage(res => {
+    if (res.from === "button") {
+      // 来自页面内转发按钮
+      console.log(res.target);
+    }
+    return {
+      title: "小活动来报名",
+      path: "pages/index/index"
+    };
+  });
+
+  useShareTimeline(res => {
+    if (res.from === "button") {
+      // 来自页面内转发按钮
+      console.log(res.target);
+    }
+    return {
+      title: "小活动来报名"
+    };
+  });
+  const tabList = [{ title: "发件箱" }, { title: "收件箱" }];
+
+  const handleToggleTab = value => setCurrent(value);
+
+  const toNewActivity = () => {
+    Taro.navigateTo({ url: "/pages/publish/index" });
+  };
+
   return (
-    <View className="index">
-      <AtTabBar
-        tabList={[{ title: "发件箱" }, { title: "收件箱" }]}
-        onClick={value => setCurrent(value)}
-        current={current}
-      />
+    <View className={styles.index}>
+      <AtTabs current={current} tabList={tabList} onClick={handleToggleTab}>
+        <AtTabsPane current={current} index={0}>
+          <View className={styles.tabPageCon}>标签页一的内容</View>
+        </AtTabsPane>
+        <AtTabsPane current={current} index={1}>
+          <View className={styles.tabPageCon}>标签页二的内容</View>
+        </AtTabsPane>
+      </AtTabs>
+      <AtButton onClick={toNewActivity} className={styles.footerBtn}>
+        新建活动
+      </AtButton>
     </View>
   );
 }
