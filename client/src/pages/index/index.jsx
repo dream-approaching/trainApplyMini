c82ff51b80c12e4301ca63283fc9ba709e449d91
zhengjinshou <zhengjinshou@qtrade.com.cn>
2020-12-16 23:14:46 +0800

    .

--- client/src/pages/index/index.jsx
+++ client/src/pages/index/index.jsx
@@ -1,33 +1,23 @@
-import Taro, { Component } from "@tarojs/taro";
-  import { AtTabBar } from 'taro-ui'
-import { View, Text } from "@tarojs/components";
-import "./index.less";
+import Taro, { useState } from "@tarojs/taro";
+import { AtTabBar } from "taro-ui";
+import { View } from "@tarojs/components";
+import styles from "./index.module.less";
 
-import Login from "../../components/login/index.weapp";
+export default function Index() {
+  const [current, setCurrent] = useState(0);
 
-export default class Index extends Component {
-  config = {
-    navigationBarTitleText: "首页"
-  };
-
-  componentWillMount() {}
-
-  componentDidMount() {
-    
-    console.log('window', this.$router)
-  }
-
-  componentWillUnmount() {}
-
-  componentDidShow() {}
-
-  componentDidHide() {}
-
-  render() {
-    return (
-      <View className="index">
-        <Login />
-      </View>
-    );
-  }
+  console.log("%c zjs styles:", "color: #0e93e0;background: #aaefe5;", styles);
+  return (
+    <View className="index">
+      <AtTabBar
+        tabList={[{ title: "发件箱" }, { title: "收件箱" }]}
+        onClick={value => setCurrent(value)}
+        current={current}
+      />
+    </View>
+  );
 }
+
+Index.config = {
+  navigationBarTitleText: "首页"
+};
